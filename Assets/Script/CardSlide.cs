3fb5b119dcadcb69c22eb1ec3b75af360033b897
tooyama <toyana@dcl.cs.waseda.ac.jp>
2015-08-20 13:30:35 +0900

    Update

---
+++ Assets/Script/CardSlide.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CardSlide : MonoBehaviour 
+{
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+
+    }
+
+    public void CardMove(int range)
+    {
+        if (!Input.GetMouseButton(0))
+        {
+            return;
+        }
+
+        transform.Translate(range,0,0);
+    }
+}
---
+++ Assets/Script/ViewCard.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ViewCard : MonoBehaviour 
+{
+    public GameObject cardText;
+
+	void Start () 
+    {
+	   
+	}
+	
+	void Update () 
+    {
+	    if (Input.GetMouseButtonDown(0))
+        {
+            cardText.SetActive(!cardText.activeSelf);
+        }
+	}
+}
