8ae0ac2ac0a7ffbf436dbed40e5be06ca672a954
tooyama <toyama@dcl.cs.waseda.ac.jp>
2016-03-11 03:25:06 +0900

    UI追加

---
+++ Assets/Script/HPCount.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class HPCount : MonoBehaviour 
+{
+    public float hp = 12.0f;
+    private float defaultHp;
+    private Text hpText;
+    private Image circleImage;
+
+	void Start () 
+    {
+        defaultHp = hp;
+        circleImage = transform.FindChild("HPCircle").GetComponent<Image>();
+        hpText = transform.FindChild("HPCircle").FindChild("HPCount").GetComponent<Text>();
+        hpText.text = hp.ToString();
+	}
+	
+	void Update () 
+    {
+	    if(Input.anyKeyDown)
+        {
+            ChangeState(1);
+        }
+	}
+
+    void ChangeState(float damage)
+    {
+        if(hp > 0)
+        {
+            hp -= damage;
+        }
+
+        circleImage.fillAmount = hp / defaultHp;
+
+        hpText.text = hp.ToString();
+    }
+}
